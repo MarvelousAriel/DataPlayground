f1eb9f77fd21d0ff4ac37bf48135d14303b2ee81
o0-0o <51746593+MarvelousAriel@users.noreply.github.com>
2021-09-14 18:28:41 +0800

    Add files via upload

---
+++ 用蒙特卡洛方法求π值.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 14 18:27:28 2021
+
+@author: Ariel
+"""
+
+# 用蒙特卡洛模拟的方法求π值
+import random
+import math
+
+n = 1000000 #随机投掷100万次
+r = 1.0 #内切圆边长设为1
+a, b = (0.0, 0.0) #中心点坐标
+x_neg, x_pos = a-r, a+r
+y_neg, y_pos = b-r, b+r
+
+count = 0.0
+for i in range(0, n):
+	x = random.uniform(x_neg, x_pos)
+	y = random.uniform(y_neg, y_pos)
+
+	if math.sqrt(x**2 + y**2) <= 1.0: #判定随机点与中心点的距离
+		count += 1
+
+result = (count / n) * 4
+print("Estimation of π is ", result)
